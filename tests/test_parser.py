a0bac9c8eccef8af914606b8a1a7ac1a702a5d95
ianagbip1oti <ianagbip1oti@gmail.com>
2018-11-02 22:55:06 +0000

    Allow multiple expressions

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,11 +3,23 @@ from keith.parser import parse
 from lark.lexer import Token
 from lark.tree import Tree
 
+hello_world_print = Tree('function_call', [
+    Token('IDENTIFIER', 'print'),
+    Tree('arguments', [Tree('literal', [Token('STRING', '"hello world"')])])
+])
+
 
 def test_parse_hello_world():
     p = 'print("hello world")'
 
-    assert parse(p) == Tree('start', [
-        Token('IDENTIFIER', 'print'),
-        Tree('arguments', [Token('STRING', '"hello world"')])
-    ])
+    assert parse(p) == Tree('compound_expr', [hello_world_print])
+
+
+def test_parse_hello_world_multi():
+    p = """
+print("hello world")
+print("hello world")
+    """
+
+    assert parse(p) == Tree('compound_expr',
+                            [hello_world_print, hello_world_print])
