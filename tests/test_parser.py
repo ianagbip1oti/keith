7a7587b5506f97e78a8862e86f0a67ddeb2ab07b
ianagbip1oti <ianagbip1oti@gmail.com>
2018-11-05 18:11:54 +0000

    Add function def parsing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,8 +3,17 @@ from keith.parser import parse
 from lark.lexer import Token
 from lark.tree import Tree
 
+
+def identifier(i):
+    return Token('IDENTIFIER', i)
+
+
+def parameters(*args):
+    return Tree('parameters', [identifier(a) for a in args])
+
+
 hello_world_print = Tree('function_call', [
-    Token('IDENTIFIER', 'print'),
+    identifier('print'),
     Tree('arguments', [Tree('literal', [Token('STRING', '"hello world"')])])
 ])
 
@@ -31,3 +40,33 @@ print("hello world") # This is a comment
 """
 
     assert parse(p) == Tree('compound_expr', [hello_world_print])
+
+
+def test_parse_hello_world_function():
+    p = """
+def hello_world():
+   print("hello world")
+"""
+
+    assert parse(p) == Tree('compound_expr', [
+        Tree('function_def', [
+            identifier('hello_world'),
+            parameters(),
+            Tree('compound_expr', [hello_world_print])
+        ])
+    ])
+
+
+def test_parse_hello_world_function_with_parameters():
+    p = """
+def hello_world(a, b):
+   print("hello world")
+"""
+
+    assert parse(p) == Tree('compound_expr', [
+        Tree('function_def', [
+            Token('IDENTIFIER', 'hello_world'),
+            parameters('a', 'b'),
+            Tree('compound_expr', [hello_world_print])
+        ])
+    ])
